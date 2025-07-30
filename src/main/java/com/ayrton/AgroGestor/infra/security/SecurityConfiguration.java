e4ba5de3b6bcb5ba0b3584bfc26aecc2ebfd4648
ayrton-nhaguaga <ayrtonjuliojaimenhaguaga>
2025-07-30 17:57:03 +0200

    .

--- src/main/java/com/ayrton/AgroGestor/infra/security/SecurityConfiguration.java
+++ src/main/java/com/ayrton/AgroGestor/infra/security/SecurityConfiguration.java
@@ -1,10 +1,65 @@
 package com.ayrton.AgroGestor.infra.security;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+
+    @Autowired
+    private SecurityFilter securityFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        // Swagger - permitir acesso sem autenticação
+                        .requestMatchers(
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html"
+                        ).permitAll()
+
+                        // autorizar endpoint de login para todos
+                        .requestMatchers(HttpMethod.POST, "/api/auth/login").permitAll()
+
+                        // autorizar endpoint de registrar para teste
+                        .requestMatchers(HttpMethod.POST, "api//auth/register").permitAll()
+
+                        // apenas ADMIN pode acessar estes
+                        .requestMatchers(HttpMethod.POST, "/api/crops", "/api/employees", "/api/crops", "/api/promotions", "/api/resources", "/api/farms", "/api/fields", "/api/input_applications").hasRole("ADMIN")
+
+                        // engenheiro tem a permissao exclusiva de:
+                        .requestMatchers(HttpMethod.POST, "/api/input_applications").hasRole("ENGINEER")
+
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
